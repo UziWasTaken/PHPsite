680f50abb58ba006ea7f88b51064e266dd4a14cb
Uzi <122372890+UziWasTaken@users.noreply.github.com>
2025-02-05 17:33:05 -0600

    Remove potentially conflicting Babel configuration

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['res.cloudinary.com'],
-  },
-  // Add webpack configuration
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-    };
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig 
