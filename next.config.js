0d3683617f7cbefd43531b5678ec0d235e6ef4d4
Uzi <122372890+UziWasTaken@users.noreply.github.com>
2025-02-05 17:31:16 -0600

    handle image domains and webpack issues fixed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  // Add webpack configuration
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
