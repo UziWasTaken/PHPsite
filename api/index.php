d431f7db3b4c9408eae6d074cb05438d27372c8b
Uzi <122372890+UziWasTaken@users.noreply.github.com>
2025-02-05 01:24:27 -0600

    explicit PHP configuration

--- api/index.php
+++ api/index.php
@@ -1,8 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header('Content-Type: application/json');
-http_response_code(200);
 echo json_encode([
-    'status' => 'success',
-    'message' => 'API is working',
-    'php_version' => PHP_VERSION
+    'status' => 'ok',
+    'time' => time()
 ]); 
